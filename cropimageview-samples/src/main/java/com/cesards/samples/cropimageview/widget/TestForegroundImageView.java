f178559fac988156232bfc7d1a9b85a6c1788017
César Díez Sánchez <cesar.diez@keradgames.com>
2015-09-26 20:39:30 +0200

    changed ForegroundView for a dependency

--- cropimageview-samples/src/main/java/com/cesards/samples/cropimageview/widget/TestForegroundImageView.java
+++ cropimageview-samples/src/main/java/com/cesards/samples/cropimageview/widget/TestForegroundImageView.java
@@ -20,6 +20,7 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
+import com.cesards.android.foregroundviews.ForegroundImageView;
 import hugo.weaving.DebugLog;
 
 /**
